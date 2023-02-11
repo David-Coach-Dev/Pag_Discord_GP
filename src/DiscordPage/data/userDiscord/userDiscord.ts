c615f01f2ce7a6ef041022a22351b5ac55c7086c
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-02-11 01:53:17 -0500

    act

---
+++ src/DiscordPage/data/userDiscord/userDiscord.ts
@@ -0,0 +1,121 @@
+import user from "@/discordPage/assets/img/user.png";
+import Dc_Dev from "@/discordPage/assets/img/Dc_Dev.png";
+
+export const userDiscord = [
+  {
+    id:"0",
+    avatar: user,
+    nombre: "server",
+    numero: "0000",
+    online: true,
+    isBot: false,
+  },
+  {
+    id:"1",
+    avatar: Dc_Dev,
+    nombre: "Dc Dev",
+    numero: "0001",
+    online: true,
+    isBot: false,
+  },
+  
+  {
+    id:"2",
+    avatar: Dc_Dev,
+    nombre: "Dc Dev Bot",
+    numero: "0002",
+    online: true,
+    isBot: true,
+  },
+  {
+    id:"3",
+    avatar: user,
+    nombre:"user_3",
+    numero: "0003",
+    online: false,
+    isBot: false,
+  },
+  {
+    id:"4",
+    avatar: user,
+    nombre:"user_4",
+    numero: "0004",
+    online: false,
+    isBot: false,
+  },
+  {
+    id:"5",
+    avatar: user,
+    nombre:"user_5",
+    numero: "0005",
+    online: false,
+    isBot: false,
+  },
+  {
+    id:"6",
+    avatar: user,
+    nombre:"user_6",
+    numero: "0006",
+    online: false,
+    isBot: false,
+  },
+  {
+    id:"7",
+    avatar: user,
+    nombre:"user_7",
+    numero: "0007",
+    online: false,
+    isBot: false,
+  },
+  {
+    id:"8",
+    avatar: user,
+    nombre:"user_8",
+    numero: "0008",
+    online: false,
+    isBot: false,
+  },
+  {
+    id:"9",
+    avatar: user,
+    nombre:"user_9",
+    numero: "0009",
+    online: false,
+    isBot: false,
+  },
+  {
+    id:"10",
+    avatar: user,
+    nombre:"user_10",
+    numero: "00010",
+    online: false,
+    isBot: false,
+  },
+  {
+    id:"11",
+    avatar: user,
+    nombre:"user_11",
+    numero: "00011",
+    online: false,
+    isBot: false,
+  },
+  {
+    id:"12",
+    avatar: user,
+    nombre:"user_12",
+    numero: "00012",
+    online: false,
+    isBot: false,
+  },
+  
+]
+/*template
+{
+  id:"",
+  avatar: user,
+  nombre:"",
+  numero: "",
+  online: false,
+  isBot: false,
+},
+*/
