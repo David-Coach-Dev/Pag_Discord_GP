de0e321ffb05e4f19a30a68935f96dfa62cec24b
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-02-11 03:05:52 -0500

    act

--- src/DiscordPage/components/UserList/UserList.tsx
+++ src/DiscordPage/components/UserList/UserList.tsx
@@ -1,40 +1,43 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { ContainerUl, RoleUL, UserUL, AvatarUL, userDiscord } from "@/DiscordPage";
-export interface PorpsUL {}
-	interface UseProps{
-		key: string;
-		avatar: string;
-		nicknameUL: string;
-		isBot?: boolean;
-	};
-const UserRowUL: FC<UseProps> = ({key, avatar, nicknameUL, isBot=false}) =>{
+
+
+interface UseProps{
+	id: string;
+	avatar: string;
+	nicknameUL: string;
+	isBot?: boolean;
+};
+
+const UserRowUL: FC<UseProps> = ({id, avatar, nicknameUL, isBot}) =>{
 	return (
-	<UserUL key={key}>
-		<AvatarUL className={isBot ? 'bot':''}>
-			<img src={avatar}/>
-		</AvatarUL>
-		<strong>
-			{nicknameUL}
-		</strong>
-		{isBot && <span>Bot</span>}
-	</UserUL>
-	);
+    <UserUL key={id}>
+      <AvatarUL className={isBot ? "bot" : ""}>
+        <img src={avatar} />
+      </AvatarUL>
+      <strong>{nicknameUL}</strong>
+      {isBot && <span>Bot</span>}
+    </UserUL>
+  );
 }
+
+export interface PorpsUL {}
+
 export const UserList :FC<PorpsUL> = () => {
+	let d=0;
+	let nd=0;
 	return (
 		<ContainerUl>
-			{
 				<>
-					<RoleUL>Disponible - 1</RoleUL>
+					<RoleUL>Disponible - {d}</RoleUL>
 					{userDiscord.map((user) => {
-						return(user.online && <UserRowUL key={user.id} avatar={user.avatar} nicknameUL={user.nickName} isBot={user.isBot}/>)
+						return(user.online && <UserRowUL id={user.id} avatar={user.avatar} nicknameUL={user.nickName} isBot={user.isBot}/>)
 					})}
-					<RoleUL>Offline - 1</RoleUL>
+					<RoleUL>Offline - {nd}</RoleUL>
 					{userDiscord.map((user) => {
-						return (!user.online && <UserRowUL key={user.id} avatar={user.avatar} nicknameUL={user.nickName} isBot={user.isBot}/>)
+						return (!user.online && <UserRowUL id={user.id} avatar={user.avatar} nicknameUL={user.nickName} isBot={user.isBot}/>)
 					})}
 				</>
-			}
 		</ContainerUl>
 	);
 };
