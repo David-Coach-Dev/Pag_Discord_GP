84a5a0243957b26850147bd204f59b59dbbffbc4
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-02-11 13:38:35 -0500

    ACT

--- src/DiscordPage/data/userDiscord/userDiscord.ts
+++ src/DiscordPage/data/userDiscord/userDiscord.ts
@@ -1,4 +1,4 @@
-import user from "@/discordPage/assets/img/user.png";
+import user from "../../assets/img/user.png";
 import Dc_Dev from "@/discordPage/assets/img/Dc_Dev.png";
 
 export const userDiscord = [
